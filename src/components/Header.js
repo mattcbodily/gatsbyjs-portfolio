a108ee66007e1011a5682661403fd00a7c1f109e
Matthew Bodily <mattcbodily@gmail.com>
2019-12-12 22:07:03 -0700

    added portfolio image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import portfolioPicture from '../assets/images/GradHeadshots-1808.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
+                    <a href="#" className="image avatar"><img src={portfolioPicture} alt="" /></a>
+                    <h1><strong>I am Matt Bodily</strong>, a super simple<br />
                     responsive site template freebie<br />
                     crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
                 </div>
