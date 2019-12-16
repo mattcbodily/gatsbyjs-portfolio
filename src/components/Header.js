b6fb1b5f5200d8c306db10f779dbe255b788e5fe
mattcbodily <mattcbodily@gmail.com>
2019-12-15 22:43:18 -0700

    changed header component text to be different than the boilerplate

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,7 @@ class Header extends React.Component {
             <header id="header">
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={portfolioPicture} alt="" /></a>
-                    <h1><strong>Hi, I'm Matt Bodily</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <h1><strong>Hi, I'm Matt Bodily</strong>. I'm a web developer<br /></h1>
                 </div>
                 <Footer />
             </header>
